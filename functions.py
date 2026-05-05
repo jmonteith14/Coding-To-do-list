becd693598cc6f1e9621e58cea5d4fbb20134422
jmonteith14 <jmonteith14@gmail.com>
2026-05-05 13:50:57 -0300

    Text clears from entry, labels are bigger

--- functions.py
+++ functions.py
@@ -1,10 +1,10 @@
 import tkinter as tk
 
-def add_task(input_object, to_do_frame):
-    entry = input_object.get()
-    # print(entry)
-    entry_label = tk.Label(master=to_do_frame, text=entry)
-    entry_label.pack()
+
+def add_task(input_object, to_do_frame, event=None):
+    task_entry = input_object.get()
+    task_label = tk.Label(master=to_do_frame, text=task_entry)
+    task_label.pack(padx=2, pady=2)
     input_object.delete(0, tk.END)
 
 
--- main.py
+++ main.py
@@ -8,35 +8,34 @@ root.geometry("1000x500")
 root.configure(bg="#424242")
 
 to_do_frame = tk.Frame(root, bg="#E3DB62", width=200, height=400)
-to_do_label = tk.Label(master=to_do_frame, text='To-do')
-to_do_label.pack()
+to_do_label = tk.Label(master=to_do_frame, text='To-do', font="bold")
+to_do_label.pack(fill=tk.BOTH)
 to_do_frame.pack(side=tk.LEFT, padx=10, pady=10)
 to_do_frame.pack_propagate(False)
 
 in_progress_frame = tk.Frame(root, bg="#6284E3", width=200, height=400)
-in_progress_label = tk.Label(master=in_progress_frame, text='In Progress')
-in_progress_label.pack()
+in_progress_label = tk.Label(master=in_progress_frame, text='In Progress', font="bold")
+in_progress_label.pack(fill=tk.BOTH)
 in_progress_frame.pack(side=tk.LEFT, padx=10, pady=10)
 in_progress_frame.pack_propagate(False)
 
 completed_frame = tk.Frame(root, bg="#62E373", width=200, height=400)
-completed_label = tk.Label(master=completed_frame, text='Completed')
-completed_label.pack()
+completed_label = tk.Label(master=completed_frame, text='Completed', font="bold")
+completed_label.pack(fill=tk.BOTH)
 completed_frame.pack(side=tk.LEFT, padx=10, pady=10)
 completed_frame.pack_propagate(False)
 
 dropped_frame = tk.Frame(root, bg="#E37362", width=200, height=400)
-dropped_label = tk.Label(master=dropped_frame, text='Dropped')
-dropped_label.pack()
+dropped_label = tk.Label(master=dropped_frame, text='Dropped', font="bold")
+dropped_label.pack(fill=tk.BOTH)
 dropped_frame.pack(side=tk.LEFT, padx=10, pady=10)
 dropped_frame.pack_propagate(False)
 
-input_task = tk.Entry(root, width=30)
+input_task = tk.Entry(root, width=30, relief=tk.SUNKEN)
 input_task.pack(side=tk.TOP, padx=10, pady=10)
+input_task.bind("<Return>", partial(func.add_task, input_task, to_do_frame))
 
 input_button = tk.Button(root, text="Add Task", command=partial(func.add_task, input_task, to_do_frame))
 input_button.pack(side=tk.TOP, padx=10, pady=10)
-
-
 
 root.mainloop()
