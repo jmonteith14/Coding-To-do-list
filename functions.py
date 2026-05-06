4a76c91b90d8edfa05340ee62fa4809a64a5dce8
jmonteith14 <jmonteith14@gmail.com>
2026-05-06 14:04:48 -0300

    Added function to delete added tasks with right click

--- functions.py
+++ functions.py
@@ -1,5 +1,7 @@
 # Imports
 import tkinter as tk
+from functools import partial
+
 
 # Create function to add/create tasks
 def add_task(input_object, to_do_frame, event=None):
@@ -7,4 +9,9 @@ def add_task(input_object, to_do_frame, event=None):
     if task_entry != "":
         task_label = tk.Label(master=to_do_frame, text=task_entry)
         task_label.pack(padx=2, pady=2)
+        task_label.bind("<Button-3>", partial(delete_task, task_label))
         input_object.delete(0, tk.END)
+
+
+def delete_task(task, event=None):
+    task.destroy()
