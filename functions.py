83445fdaba0aeedba04d12b32da2081610769bf9
jmonteith14 <jmonteith14@gmail.com>
2026-05-05 22:39:22 -0300

    Fixed empty inputs

--- functions.py
+++ functions.py
@@ -1,11 +1,8 @@
 import tkinter as tk
 
-
 def add_task(input_object, to_do_frame, event=None):
     task_entry = input_object.get()
-    task_label = tk.Label(master=to_do_frame, text=task_entry)
-    task_label.pack(padx=2, pady=2)
-    input_object.delete(0, tk.END)
-
-
-
+    if task_entry != "":
+        task_label = tk.Label(master=to_do_frame, text=task_entry)
+        task_label.pack(padx=2, pady=2)
+        input_object.delete(0, tk.END)
